6e62a326f1fc3f358dfb90dbd65668d24e207a0c
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-14 12:43:45 +0600

    tailwind installed and error solved

--- Redux-Crush-Course-LWS/eslint.config.js
+++ Redux-Crush-Course-LWS/eslint.config.js
@@ -10,6 +10,7 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      node: true,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -30,4 +31,4 @@ export default [
       ],
     },
   },
-]
+];
