0c24d3490930998b91954b2e9d2a0cb9c26a37c9
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-14 14:22:59 +0600

    state lifted to the app component

--- Redux-Crush-Course-LWS/src/App.jsx
+++ Redux-Crush-Course-LWS/src/App.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 import Stats from "./components/Stats";
 
+
+const initialCounters = [
+  {id: 1, value: 0},
+  {id: 2, value: 5},
+]
+
 function App() {
+
+   const [counters, setCounters] = useState(initialCounters)
+  
+  const handleIncrement = (counterId) => {
+      return alert(counterId)
+      setCount(prevCount => prevCount + 1)
+    }
+  const handleDecrement = (counterId) => {
+         return alert(counterId);
+      if (count > 0) {
+        setCount(prevCount => prevCount - 1)
+      } else {
+        setCount(0)
+      }
+    }
 
 
   return (
     <div className='flex flex-col gap-8 justify-center items-center min-h-screen bg-indigo-950'>
-      <h2 className='mb-8 text-center text-4xl bg-rose-100 px-9 py-3 rounded-md'>Simple Counter Application</h2>
-      <Counter />
-      <Counter />
-      <Stats />
+      <h2 className='mb-8 text-center text-4xl bg-rose-100 px-9 py-3 rounded-md'>
+        Simple Counter Application
+      </h2>
+      {counters.map((counter) => (
+        <Counter
+          key={counter.id}
+          count={counter.value}
+          handleIncrement={() => handleIncrement(counter.id)}
+          handleDecrement={() => handleDecrement(counter.id)}
+        />
+      ))}
+
+      <Stats totalCount={10} />
     </div>
   );
 }
--- Redux-Crush-Course-LWS/src/components/Counter.jsx
+++ Redux-Crush-Course-LWS/src/components/Counter.jsx
@@ -3,33 +3,17 @@ import Button from "./Button"
 import Count from "./Count"
 
 
-const Counter = () => {
-
-  const [count, setCount] = useState(0)
-
-  const handleIncrement = () => {
-    setCount(prevCount => prevCount + 1)
-  }
-  const handleDecrement = () => {
-    if (count > 0) {
-      setCount(prevCount => prevCount - 1)
-    } else {
-      setCount(0)
-    }
-  }
-
+const Counter = ({ count, handleIncrement, handleDecrement }) => {
   return (
-    <main className="bg-purple-200 px-8 py-4 rounded-md ">
-     
-
-      <div className="bg-white shadow-md p-8 rounded-md flex flex-col justify-center items-center">
-     <Count count={count}/>
-        <div className="mt-8 flex gap-4">
-         <Button type='' handler={handleIncrement} text='Increment' />
-         <Button type='danger' handler={handleDecrement} text='Decrement' />
+    <main className='bg-purple-200 px-8 py-4 rounded-md '>
+      <div className='bg-white shadow-md p-8 rounded-md flex flex-col justify-center items-center'>
+        <Count count={count} />
+        <div className='mt-8 flex gap-4'>
+          <Button type='' handler={handleIncrement} text='Increment' />
+          <Button type='danger' handler={handleDecrement} text='Decrement' />
         </div>
-    </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 export default Counter
