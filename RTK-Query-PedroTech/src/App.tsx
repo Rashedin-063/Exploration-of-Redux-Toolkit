4f9d9b4a00cb254745568734a670cd1ad980d0fb
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-16 21:28:43 +0600

    data fetched successfully

--- RTK-Query-PedroTech/src/App.tsx
+++ RTK-Query-PedroTech/src/App.tsx
@@ -1,5 +1,16 @@
+import { useGetPostsQuery } from "./services/jsonPlaceholderApi"
 
 const App = () => {
+
+  const { data, error, isLoading } = useGetPostsQuery()
+  
+  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Error happens!</p>
+  
+
+  console.log(data);
+  
+
   return (
     <div>
       App
--- RTK-Query-PedroTech/src/store.ts
+++ RTK-Query-PedroTech/src/store.ts
@@ -4,7 +4,7 @@ import { jsonPlaceholderApi } from "./services/jsonPlaceholderApi";
 
 export const store = configureStore({
   reducer: {
-    [jsonPlaceholderApi.reducerPath]: jsonPlaceholderApi.enhanceEndpoints,
+    [jsonPlaceholderApi.reducerPath]: jsonPlaceholderApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(jsonPlaceholderApi.middleware)
 });
