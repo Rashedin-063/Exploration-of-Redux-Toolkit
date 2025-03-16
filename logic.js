26a1db8d466a5bde2288f36c5aef213168a4c750
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-16 17:05:00 +0600

    updated

--- logic.js
+++
@@ -1,9 +0,0 @@
-const numbers = [1, 3, 5, 8, 10];
-
-const index = numbers.findIndex(num => num > 4)
-
-console.log(index)
-
-
-// indexOf works for primitive values
-// findIndexOf works for non-primitive values and with a callback function
