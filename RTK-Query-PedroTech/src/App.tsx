543fb10b90b9176034cc56481b9d28ff32a2dbca
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-16 22:49:17 +0600

    data displayed in the  ui

--- RTK-Query-PedroTech/src/App.tsx
+++ RTK-Query-PedroTech/src/App.tsx
@@ -8,13 +8,48 @@ const App = () => {
   if (error) return <p>Error happens!</p>
   
 
-  console.log(data);
+  // console.log(data);
   
 
   return (
     <div>
-      App
+      <div
+        style={{
+          width: '100vw',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: '2rem',
+        }}
+      >
+        {data?.map((post) => (
+          <div
+            style={{
+              border: '1px dashed #c4c4c4',
+              padding: '8px 16px',
+              marginBottom: '16px',
+              width: '400px',
+              borderRadius: '8px',
+              backgroundColor: 'rebeccapurple'
+            }}
+          >
+            <p style={{ fontSize: '18px' }}>
+              {' '}
+              <span style={{ textDecoration: 'underline' }}>Title:</span>{' '}
+              {post.title}
+            </p>
+            <p>
+              {' '}
+              <span style={{ fontSize: '18px', textDecoration: 'underline' }}>
+                Description:
+              </span>{' '}
+              {post.body}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 export default App
